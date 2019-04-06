2767a3d70b23c0968ec379a1477b18a819d6c7af
mattsei <sei@bioscollective.com>
2019-04-06 14:30:58 +1100

    api keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
   :publishable_key => Figaro.env.stripe_publishable_key,
-  :secret_key      => Figaro.env.stripe_api_key
+  :secret_key      => Figaro.env.stripe_api_key,
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
