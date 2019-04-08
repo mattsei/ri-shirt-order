ed3ab9a64671aa980faafab67f4b8191bed88bf7
mattsei <sei@bioscollective.com>
2019-04-08 23:37:33 +1000

    mkey

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, "/home/deploy/#{fetch :application}"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/application.yml"
+append :linked_files, "config/application.yml", "config/master.key"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", ".bundle", "public/system", "public/uploads"
